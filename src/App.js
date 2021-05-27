e1323d80346ece9d6c1338d77fe8e67eb47979d6
whywesmurfing <raymond.doan94@gmail.com>
2021-05-27 13:05:51 +1000

    Updated the tests and adding additional colours

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const getBackGroundColor = ({ degree, backGroundColors }) => {
-  let string = `${degree || 135}deg`;
-
-  for (let i = 0; i < backGroundColors.length; i++) {
-    string += `, ${backGroundColors[i]}`;
-  }
+export const getBackgroundCSS = ( backgroundColors ) => {
+  let string = `background: linear-gradient(45deg,`;
+  string += backgroundColors.join(",")
+  string += ");"
 
   return string;
 };
 
 // Styling the webpage
 const StyledMain = styled.main`
-  background-image: linear-gradient(${getBackGroundColor});
+  ${props => props.backgroundCSS}
   display: flex;
   flex-direction: column;
   align-items: center;
   height: 100%;
+  width: 100%;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
 `;
 
+const updateLocalStorage = (state) => {
+  // JSON stringify state
+  const stringState = JSON.stringify(state)
+  // save string to localstorage
+  localStorage.setItem('backgroundColors', stringState)
+}
+
+const getFromLocalStorage = () => {
+  const colors = localStorage.getItem('backgroundColors')
+  if (!colors) {
+    return null
+  }
+  return JSON.parse(colors)
+}
+
 const App = () => {
   const [backgroundColors, setBackgroundColors] = useState(["#d4d4d4", "#34e791", "#000000"])
   const [degree, setDegree] = useState(45)
 
+  // Initial run
+  useEffect(() => {
+    const colors = getFromLocalStorage
+    if (colors) {
+      setBackgroundColors(colors)
+    }
+  }, [])
+
+  // Runs everytime there is a change
+  useEffect(() => {
+    updateLocalStorage(backgroundColors)
+  }, [backgroundColors])
+
   const handleColorChange = (event, i) => {
     const newColor = event.target.value
-
     const newBackgroundColors = [...backgroundColors]
-
     newBackgroundColors[i] = newColor
+    setBackgroundColors(newBackgroundColors)
+  }
+  
+  const backgroundCSS = getBackgroundCSS(backgroundColors)
+  
+  const addColor = () => {
+    // clone the existing
+    const newBackgroundColors = [...backgroundColors]
+    // add new color at the end
+    newBackgroundColors.push("#404040")
+    // set it into state
+    setBackgroundColors(newBackgroundColors)
+  }
 
+  const removeColor = (index) => {
+    // clone
+    const newBackgroundColors = [...backgroundColors]
+    // remove
+    newBackgroundColors.splice(index, 1)
+    // set to state
     setBackgroundColors(newBackgroundColors)
   }
 
@@ -44,7 +88,7 @@ const App = () => {
   }
 
   return (
-    <StyledMain backgroundColors={backgroundColors} degree={degree}>
+    <StyledMain backgroundCSS={backgroundCSS}>
       <h1>CSS Gradient Generator</h1>
       <div>
         Angle:
@@ -54,20 +98,21 @@ const App = () => {
           onChange={(event) => handleDegree(event)}
           style={{ width: "100px" }}
         />
-        {backgroundColors.map((color, index) => {
-          return (
-            <div key={index}>
-              Colour #{index + 1}:{" "}
-              <input
-                key={color}
-                type="color"
-                value={color}
-                onChange={(event) => handleColorChange(event, index)}
-              />
-            </div>
-          );
-        })}
-        {/* Colour #2: <input type="color" /> */}
+        {backgroundColors.map((color, index) => (
+          <div key={`${index}${color}`}>
+            Colour #{index + 1}:{" "}
+            <input
+              type="color"
+              value={color}
+              onChange={(event) => handleColorChange(event, index)}
+            />
+            {backgroundColors.length > 2 && <button onClick={() => removeColor(index)}>Remove Colour</button>}
+          </div>
+          )
+        )}
+        <br />
+        <button type="submit" onClick={addColor}>Add Colour</button>
+        <p>{backgroundCSS}</p>
       </div>
     </StyledMain>
   )
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,28 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import App, { getBackgroundCSS } from './App';
 
 test('renders the app', () => {
   const app = render(<App />);
   expect(app).toBeTruthy();
 });
+
+describe("getBackgroundColor", () => {
+  test('should handle single color', () => {
+    const colors = ["#fff"]
+    const output = "background: linear-gradient(45deg,#fff);"
+    expect(getBackgroundCSS(colors)).toBe(output)
+  })
+
+  test('should handle two colors', () => {
+    const colors = ["#fff", "#000"]
+    const output = "background: linear-gradient(45deg,#fff,#000);"
+    expect(getBackgroundCSS(colors)).toBe(output)
+  })
+
+  test('should handle multiple color', () => {
+    const colors = ["#fff", "#000", "#aaa"]
+    const output = "background: linear-gradient(45deg,#fff,#000,#aaa);"
+    expect(getBackgroundCSS(colors)).toBe(output)
+  })
+
+})
